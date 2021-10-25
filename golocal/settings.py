81c28617aae116651abdd2b389df0d60cf9a5a33
Ubuntu <ubuntu@ip-172-31-22-206.us-east-2.compute.internal>
2021-10-25 10:18:26 +0000

    hosted on aws

--- golocal/settings.py
+++ golocal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,10 +132,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+#STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, '/home/ubuntu/django/site/assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -146,3 +149,5 @@ MEDIA_ROOT = BASE_DIR / 'media/'
 
 STRIPE_PUB_KEY = 'pk_test_51JniR8KwORte8SQ9NNeUG4DeRjDXa5lR9szPE5mhg7E4t58HwERV0hKrYDsINfAtJzsAfglt14vP5idNqDfnbveF00DzNsBM2T'
 STRIPE_SECRET_KEY = 'sk_test_51JniR8KwORte8SQ9ptZpnMfLgFUT4rIPorhhM86b1Kl41OnR82xSud3DnjBDB5QDVLVWBjuqmvDtXypWB2A5MjNa00SM2IlWHB'
+
+ALLOWED_HOSTS=['ec2-18-224-172-190.us-east-2.compute.amazonaws.com']
