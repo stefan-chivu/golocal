af1bb9751191f2618d52651a2dc7037eae723664
Stefan Chivu <stefan.chivu1@yahoo.com>
2021-11-15 11:36:27 +0000

    made some UI improvements

--- apps/core/views.py
+++ apps/core/views.py
@@ -5,7 +5,7 @@ from apps.product.models import Product
 
 # Create your views here.
 def frontpage(request):
-    newest_products = Product.objects.all()[0:8]
+    newest_products = Product.objects.all()[0:4]
     return render(request, 'core/frontpage.html', {'newest_products': newest_products})
 
 def contact(request):
